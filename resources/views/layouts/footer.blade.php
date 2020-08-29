443e5b251301f2a74274e9fcb622eee6d5a4b777
molnarjg <molnarjg@gmail.com>
2020-08-29 22:21:31 +1200

    commit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,8 @@
 <div class="container">
+    <div class="footer">
         <img class="footer-img" src="{{ url('storage/images/icon_Insta.svg') }}" width=136 alt="Instagram" loading="lazy" ALIGN=CENTER>
         <img class="footer-img" src="{{ url('storage/images/icon_Linkedin_.svg') }}" width=136 alt="LinkedIn" loading="lazy" ALIGN=CENTER>
         <img class="footer-img" src="{{ url('storage/images/icon_envelope.svg') }}" width=136 alt="Email" loading="lazy" ALIGN=CENTER>
         <img class="footer-img" src="{{ url('storage/images/icon_behance_.svg') }}" width=136 alt="Behance" loading="lazy" ALIGN=CENTER>
+    </div>
 </div>
