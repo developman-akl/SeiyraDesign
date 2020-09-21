8d0d6ef5288338dba90f112c6f4c80f38451764d
molnarjg <molnarjg@gmail.com>
2020-09-21 18:07:57 +1200

    navbar-toggler

--- resources/views/sections/nav.blade.php
+++ resources/views/sections/nav.blade.php
@@ -1,15 +1,19 @@
-<div id="welcome" class="nav-bar sticky" data-id="1">
+<nav id="welcome" class="navbar navbar-expand-md sd-navbar navbar-dark" data-id="1">
     <div class="container">
         <a class="js-anchor-link" href="#welcome">
-            <img class="nav-logo-img" src="{{ asset('/storage/images/SeyraDesignLogo.svg') }}" width=163 alt="Seiyra Design Logo" loading="lazy" ALIGN=CENTER>
+            <img class="nav-logo-img" src="{{ asset('/storage/images/SeyraDesignLogo.svg') }}"
+                width=163 alt="Seiyra Design Logo" loading="lazy">
         </a>
-        <nav>
-            <ul>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse sd-nav" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
                 <li><a class="js-anchor-link" href="#welcome">Home</a></li>
                 <li><a class="js-anchor-link" href="#services">Services</a></li>
                 <li><a class="js-anchor-link" href="#portfolio">Portfolio</a></li>
                 <li><a class="js-anchor-link" href="#contact">Contact</a></li>
             </ul>
-        </nav>
+        </div>
     </div>
-</div>
+</nav>
