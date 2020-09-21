8659cb218fcdae3ca55781d0b5703b024abd5e81
molnarjg <molnarjg@gmail.com>
2020-09-21 18:40:35 +1200

    services responsive

--- resources/views/sections/services.blade.php
+++ resources/views/sections/services.blade.php
@@ -7,34 +7,24 @@
         <p>{{ setting('services.description') }}</p>
     </div>
 
-    <div class="service-images">
-        <div class="image-wrapper">
-            <a href="">
-                <img class="img-fluid" src="{{ asset('storage/images/service1.png') }}" 
-                    alt="UX/UI Design" loading="lazy" ALIGN=CENTER>
-            </a>
-            <h3>UX/UI Design</h3>
+    <div class="service-images row">
+        {{-- <div class="image-wrapper d-flex flex-row justify-content-stretch"> --}}
+            <div class="column">
+                <img class="" src="{{ asset('storage/images/service1.png') }}" alt="UX/UI Design" loading="lazy" ALIGN=CENTER>
+                <h2>UX/UI Design</h2>
+            </div>
+            <div class="column">
+                <img class="" src="{{ asset('storage/images/service2.png') }}" alt="Photo Editing" loading="lazy" ALIGN=CENTER>
+                <h2>Photo Editing</h2>
+            </div>
+            <div class="column">
+                <img class="" src="{{ asset('storage/images/service3.png') }}" alt="Social Media Creative Design" loading="lazy" ALIGN=CENTER>
+                <h2>Social Media Creative Design</h2>
+            </div>
+            <div class="column">
+                <img class="" src="{{ asset('storage/images/service4.png') }}" alt="Logo Design" loading="lazy" ALIGN=CENTER>
+                <h2>Logo Design</h2>
+            </div>
         </div>
-        <div class="image-wrapper">
-            <a href="">
-                <img class="img-fluid" src="{{ asset('storage/images/service2.png') }}" 
-                    alt="Photo Editing" loading="lazy" ALIGN=CENTER>
-            </a>
-            <h3>Photo Editing</h3>
-        </div>
-        <div class="image-wrapper">
-            <a href="">
-                <img class="img-fluid" src="{{ asset('storage/images/service3.png') }}" 
-                    alt="Social Media Creative Design" loading="lazy" ALIGN=CENTER>
-            </a>
-            <h3>Social Media Creative Design</h3>
-        </div>
-        <div class="image-wrapper">
-            <a href="">
-                <img class="img-fluid" src="{{ asset('storage/images/service4.png') }}" 
-                    alt="Logo Design" loading="lazy" ALIGN=CENTER>
-            </a>
-            <h3>Logo Design</h3>
-        </div>
-    </div>
+    {{-- </div> --}}
 </div>
