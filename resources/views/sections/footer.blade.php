3f6b61e66120694023e2e42b1dd51671b36cd60a
molnarjg <molnarjg@gmail.com>
2020-10-13 16:45:20 +1300

    pulsing footer icons

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -2,19 +2,19 @@
     <div class="container">
         <div class="footer-icons">
             <a href="{{ setting('site.instagram') }}" target="_blank">
-                <img class="img-fluid " src="{{ asset('storage/images/icon_Insta.svg') }}" 
+                <img class="img-fluid pulse" src="{{ asset('storage/images/icon_Insta.svg') }}" 
                     alt="Instagram" loading="lazy" title="Instagram">
             </a>
             <a href="{{ setting('site.linkedin') }}" target="_blank">
-                <img class="img-fluid " src="{{ asset('storage/images/icon_Linkedin_.svg') }}" 
+                <img class="img-fluid pulse" src="{{ asset('storage/images/icon_Linkedin_.svg') }}" 
                     alt="LinkedIn" loading="lazy" title="LinkedIn">
             </a>
             <a href="mailto:{{ setting('site.email') }}">
-                <img class="img-fluid " src="{{ asset('storage/images/icon_envelope.svg') }}" 
+                <img class="img-fluid pulse" src="{{ asset('storage/images/icon_envelope.svg') }}" 
                     alt="Email" loading="lazy" title="Email">
             </a>
             <a href="{{ setting('site.behance') }}" target="_blank">
-                <img class="img-fluid " src="{{ asset('storage/images/icon_behance_.svg') }}" 
+                <img class="img-fluid pulse" src="{{ asset('storage/images/icon_behance_.svg') }}" 
                     alt="Behance" loading="lazy" title="Béhance">
             </a>
         </div>
