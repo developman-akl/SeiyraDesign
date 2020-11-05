f24b9e25f543370b7f18549380eb3878b6a77101
molnarjg <molnarjg@gmail.com>
2020-11-05 18:37:51 +1300

    UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,8 +18,8 @@ class UsersTableSeeder extends Seeder
             $role = Role::where('name', 'admin')->firstOrFail();
 
             User::create([
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
+                'name'           => 'Nora',
+                'email'          => 'nora@seiyradesign.com',
                 'password'       => bcrypt('password'),
                 'remember_token' => Str::random(60),
                 'role_id'        => $role->id,
